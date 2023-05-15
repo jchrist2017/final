b702bf494ef703ef2bae598bc997ce712a8269c3
jchrist2017 <126637331+jchrist2017@users.noreply.github.com>
2023-05-15 12:00:36 +0800

    Add files via upload

---
+++ FINAL/RegisterControl1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class RegisterControl1 : UserControl
+    {
+        public RegisterControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        private
+
+        }
+
+        void { label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
